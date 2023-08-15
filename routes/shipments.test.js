fa39de01804839b9f2e2a58719106cee9f9c98a8
Jason Johnson <jasjoh@live.com>
2023-08-15 15:36:36 -0700

    added mocks + code reviewed

--- routes/shipments.test.js
+++ routes/shipments.test.js
@@ -1,11 +1,17 @@
 "use strict";
 
+const shipItApi = require("../shipItApi");
+shipItApi.shipProduct = jest.fn();
+
 const request = require("supertest");
 const app = require("../app");
 
 
 describe("POST /", function () {
   test("valid", async function () {
+    // mocking response from shipProduct() to avoid call to external API
+    // shipProduct() returns resp.data.receipt.shipId
+    shipItApi.shipProduct.mockReturnValue(12345);
     const resp = await request(app).post("/shipments").send({
       productId: 1000,
       name: "Test Tester",
@@ -13,7 +19,7 @@ describe("POST /", function () {
       zip: "12345-6789",
     });
 
-    expect(resp.body).toEqual({ shipped: expect.any(Number) });
+    expect(resp.body).toEqual({ shipped: 12345 });
   });
 
   test("throws error if empty request body", async function () {
--- shipItApi.test.js
+++ shipItApi.test.js
@@ -1,11 +1,24 @@
 "use strict";
 
+const AxiosMockAdapter = require(
+  "axios-mock-adapter");
+const axios = require("axios");
+const axiosMock = new AxiosMockAdapter(axios);
+
+const { SHIPIT_SHIP_URL } = require("./shipItApi");
+
+
 const {
   shipProduct,
 } = require("./shipItApi");
 
 
 test("shipProduct", async function () {
+  // mocking response from SHIPIT API to avoid call
+  // SHIPIT API returns a receipt object, but we are only testing shipId
+  axiosMock.onPost(`${SHIPIT_SHIP_URL}`).reply(201,
+    { receipt: { shipId: 12345 }});
+
   const shipId = await shipProduct({
     productId: 1000,
     name: "Test Tester",
@@ -13,5 +26,5 @@ test("shipProduct", async function () {
     zip: "12345-6789",
   });
 
-  expect(shipId).toEqual(expect.any(Number));
+  expect(shipId).toEqual(12345);
 });
